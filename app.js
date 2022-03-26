24fe34c76498f3b92fcc2a98e786eae61435a6a6
Dewayne stegall <73716855+DewayneStegall@users.noreply.github.com>
2022-03-26 00:01:01 -0400

    Update folders

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+let answer1 = prompt("Do You Like Motorcycles?");
+let answer2 = prompt("What is your favorite Brand of Motorcycle?")
+
+    if ( answer1 === "yes") {
+        document.write("<li> Thats a great choice, here are some others too </li>")
+    }
+
+
+        else {
+            document.write("<li> I think you should tkae another look... </li>"); }
