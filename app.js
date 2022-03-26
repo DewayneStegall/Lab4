aadd26de5618c1e50152cc7ed7e28dcd17a337b2
Dewayne stegall <73716855+DewayneStegall@users.noreply.github.com>
2022-03-26 01:31:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-
-
-let answer1 = prompt("Do You Like Motorcycles?");
-let answer2 = prompt("What is your favorite Brand of Motorcycle?")
-
-    if ( answer1 === "yes") {
-        document.write("<li> Thats a great choice, here are some others too </li>")
+function favBike () {
+    let favBike = prompt("What is your favorite Motorcycle?");
+    document.write("That's cool! "+ favBike + " deserved an Trophy!");
+  }
+  
+  function R1() {
+    let answer;
+    let guesses = 0;
+  
+    do {
+      guesses++;
+      answer = prompt("Guess how many R1 Types there are?");
+    } while (parseInt(answer) !== 6)
+  
+    if (parseInt(answer) === 6) {
+      document.write("It took you " + guesses + " time(s) to guess the correct answer.")
     }
+  
+  }
+  
+  function rating() {
+    let output = '';
+    let rating = prompt("Scale of 1-5 how much do you love to ride Motorcycels?");
+  
+    for (let i = 0; i < rating; i++) {
+      output += "<img src='/2000000037.jpg' />";
+    }
+  
+    document.write(output);
+  
+  }
 
-
-        else {
-            document.write("<li> I think you should tkae another look... </li>"); }
